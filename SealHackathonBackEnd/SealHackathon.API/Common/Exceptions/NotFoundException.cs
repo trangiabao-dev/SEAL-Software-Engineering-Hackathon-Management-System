970bb4f1e19d76fc3ed4f8f4cbd6133dca5ded1e
Trần Gia Bảo <giabao991122@gmail.com>
2026-05-21 02:16:13 +0700

    u

--- SealHackathonBackEnd/SealHackathon.API/Common/Exceptions/NotFoundException.cs
+++ SealHackathonBackEnd/SealHackathon.API/Common/Exceptions/NotFoundException.cs
@@ -1,6 +1,12 @@
-﻿namespace SealHackathon.API.Common.Exceptions
+namespace SealHackathon.API.Common.Exceptions
 {
-    public class NotFoundException
+    public class NotFoundException : AppException
     {
+        public NotFoundException(int resourceName, object id)
+            : base(404, $"{resourceName} with id '{id}' was not found.")
+        { }
+
+        public NotFoundException(string message) : base(404, message)
+        { }
     }
 }
--- SealHackathonBackEnd/SealHackathon.API/Models/Entities/SealDbContext.cs
+++ SealHackathonBackEnd/SealHackathon.API/Models/Entities/SealDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
@@ -52,10 +52,6 @@ public partial class SealDbContext : DbContext
     public virtual DbSet<Topic> Topics { get; set; }
 
     public virtual DbSet<Track> Tracks { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-V5C19DP0;Database=SEAL_Hackathon_DB;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
