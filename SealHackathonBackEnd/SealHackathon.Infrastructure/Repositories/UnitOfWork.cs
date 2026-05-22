22bc68f5481b830d8ebee5baa5773b2c0090596c
Trần Gia Bảo <giabao991122@gmail.com>
2026-05-22 18:05:03 +0700

    Update UnitOfWork.cs

--- SealHackathonBackEnd/SealHackathon.Infrastructure/Repositories/UnitOfWork.cs
+++ SealHackathonBackEnd/SealHackathon.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,13 +1,12 @@
 ﻿using SealHackathon.Domain.Interfaces.Repositories;
 using SealHackathon.Infrastructure.Data;
-using System.Collections;
 
 namespace SealHackathon.Infrastructure.Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly SealDbContext _context;
-        private Hashtable _repositories;
+        private readonly Dictionary<string, object> _repositories = new();
 
         public UnitOfWork(SealDbContext context)
         {
@@ -16,20 +15,13 @@ namespace SealHackathon.Infrastructure.Repositories
 
         public IGenericRepository<T> GetRepository<T>() where T : class
         {
-            if (_repositories == null)
-                _repositories = new Hashtable();
-
             var type = typeof(T).Name;
 
-            // Nếu trong Hashtable chưa có Repository của bảng này thì mới tạo mới
             if (!_repositories.ContainsKey(type))
             {
-                var repositoryType = typeof(GenericRepository<>);
-                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _context);
-                _repositories.Add(type, repositoryInstance);
+                _repositories[type] = new GenericRepository<T>(_context);
             }
 
-            // Nếu có rồi thì lấy ra dùng luôn
             return (IGenericRepository<T>)_repositories[type]!;
         }
 
