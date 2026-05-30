ae9826ffb2d1db74e5a6936c7b7e8d5bbb6478f7
Trần Gia Bảo <giabao991122@gmail.com>
2026-05-30 16:38:06 +0700

    u

---
+++ SealHackathonBackEnd/SealHackathon.Application/DTOs/Team/UpdateTeamRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SealHackathon.Application.DTOs.Team
+{
+    public class UpdateTeamRequest
+    {
+        public string TeamName { get; set; } = null!;
+        public string University { get; set; } = null!;
+        public string? GithubRepoLink { get; set; }
+    }
+}
---
+++ SealHackathonBackEnd/SealHackathon.Application/Services/Interfaces/ITeamService.cs
@@ -0,0 +1,11 @@
+﻿using SealHackathon.Application.DTOs.Team;
+
+namespace SealHackathon.Application.Services.Interfaces
+{
+    public interface ITeamService
+    {
+        Task<TeamDetailDto> CreateTeamAsync(CreateTeamRequest request, Guid leaderId);
+        Task<TeamDetailDto> UpdateTeamAsync(Guid teamId, UpdateTeamRequest request, Guid leaderId);
+        Task<TeamDetailDto> GetByIdAsync(Guid teamId);
+    }
+}
