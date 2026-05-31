aa4af11e111594939da7724b3cf127eb4565a85e
Trần Gia Bảo <giabao991122@gmail.com>
2026-05-31 11:54:50 +0700

    updata DB

---
+++ SealHackathonBackEnd/SealHackathon.Application/Services/Implementations/TeamService.cs
@@ -0,0 +1,31 @@
+﻿using SealHackathon.Application.DTOs.Team;
+using SealHackathon.Application.Services.Interfaces;
+using SealHackathon.Domain.Interfaces.Repositories;
+
+namespace SealHackathon.Application.Services.Implementations
+{
+    public class TeamService : ITeamService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public TeamService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<TeamDetailDto> CreateTeamAsync(CreateTeamRequest request, Guid leaderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TeamDetailDto> GetByIdAsync(Guid teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TeamDetailDto> UpdateTeamAsync(Guid teamId, UpdateTeamRequest request, Guid leaderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
