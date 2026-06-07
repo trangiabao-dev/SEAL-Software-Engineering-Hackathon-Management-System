a6c9f3ac490e4e506bc4fbd82e2f0de1389f6b0a
thucmai-cmd <thucskt789@gmail.com>
2026-06-07 14:03:49 +0700

    U

--- SealHackathonBackEnd/SealHackathon.Application/DTOs/Auth/EventStaffResponse.cs
+++ SealHackathonBackEnd/SealHackathon.Application/DTOs/Auth/EventStaffResponse.cs
@@ -5,6 +5,7 @@ namespace SealHackathon.Application.DTOs.Auth
         public Guid AccountId { get; set; }
         public int EventId { get; set; }
         public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
         public string EventRole { get; set; } = null!;
         public string Status { get; set; } = null!;
         public string? JudgeType { get; set; }
