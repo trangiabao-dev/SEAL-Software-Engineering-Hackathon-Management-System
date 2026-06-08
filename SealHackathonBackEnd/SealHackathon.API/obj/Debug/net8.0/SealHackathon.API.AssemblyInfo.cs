ebdd888cde6ae0153da556e6b053c42099421e61
Trần Gia Bảo <giabao991122@gmail.com>
2026-06-08 11:02:38 +0700

    Stop tracking ignored local files

--- SealHackathonBackEnd/SealHackathon.API/obj/Debug/net8.0/SealHackathon.API.AssemblyInfo.cs
+++ SealHackathonBackEnd/SealHackathon.API/obj/Debug/net8.0/SealHackathon.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SealHackathon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b1f53631870d313858d0b236a99fbba8959a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f541a1bd3a6ea45fdc9e918d0a197726331728c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SealHackathon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SealHackathon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
