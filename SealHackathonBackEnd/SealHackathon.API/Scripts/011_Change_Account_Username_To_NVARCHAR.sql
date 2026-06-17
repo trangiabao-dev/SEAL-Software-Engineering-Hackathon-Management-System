d280655b2cbbbf711f4a21cb713a4990bfa550c8
Trần Gia Bảo <giabao991122@gmail.com>
2026-06-17 18:27:03 +0700

    u

--- SealHackathonBackEnd/SealHackathon.API/Scripts/011_Change_Account_Username_To_NVARCHAR.sql
+++ SealHackathonBackEnd/SealHackathon.API/Scripts/011_Change_Account_Username_To_NVARCHAR.sql
@@ -2,6 +2,7 @@
 -- Dung NVARCHAR de luu duoc ten co dau tieng Viet, tranh bi loi ky tu khi tao Leader, Mentor, Judge.
 
 DECLARE @UsernameConstraintName SYSNAME;
+DECLARE @Sql NVARCHAR(MAX);
 
 SELECT @UsernameConstraintName = kc.name
 FROM sys.key_constraints kc
@@ -17,7 +18,8 @@ WHERE kc.parent_object_id = OBJECT_ID(N'dbo.Account')
 
 IF @UsernameConstraintName IS NOT NULL
 BEGIN
-    EXEC(N'ALTER TABLE dbo.Account DROP CONSTRAINT ' + QUOTENAME(@UsernameConstraintName));
+    SET @Sql = N'ALTER TABLE dbo.Account DROP CONSTRAINT ' + QUOTENAME(@UsernameConstraintName);
+    EXEC(@Sql);
 END;
 
 IF EXISTS (
