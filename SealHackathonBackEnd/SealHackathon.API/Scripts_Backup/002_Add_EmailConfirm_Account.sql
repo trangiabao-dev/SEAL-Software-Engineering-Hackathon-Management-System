7cdac1344450f1e9d5b1a505240d98420e2c29cf
Trần Gia Bảo <giabao991122@gmail.com>
2026-07-01 20:52:09 +0700

    u

--- SealHackathonBackEnd/SealHackathon.API/Scripts_Backup/002_Add_EmailConfirm_Account.sql
+++
@@ -1,6 +0,0 @@
--- 002_Add_EmailConfirm_Account.sql
--- Thêm 2 field xác nhận email vào bảng Account
-
-ALTER TABLE Account
-ADD EmailConfirmToken VARCHAR(500) NULL,
-    TokenExpiresAt    DATETIME2    NULL;
