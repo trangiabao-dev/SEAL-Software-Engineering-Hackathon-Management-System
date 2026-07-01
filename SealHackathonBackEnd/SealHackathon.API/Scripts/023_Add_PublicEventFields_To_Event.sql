2acc6d45707ef82235ea4bd308f35b20daec0db4
Trần Gia Bảo <giabao991122@gmail.com>
2026-07-01 18:33:33 +0700

    u

--- SealHackathonBackEnd/SealHackathon.API/Scripts/023_Add_PublicEventFields_To_Event.sql
+++
@@ -1,6 +0,0 @@
--- Add new fields for Public Event and FE-BE contract requirements
-ALTER TABLE [Event]
-ADD [BannerUrl] VARCHAR(1000) NULL,
-    [Location] NVARCHAR(500) NULL,
-    [IsOnline] BIT NULL;
-GO
